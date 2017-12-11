392c166d865d01c6fb8c1528d8d2ac9543b0e093
ipolnik <ipolnik@yahoo.com>
2017-12-12 00:36:01 +0600

    second change

--- src/main/java/com/demo/pages/LoginPage.java
+++ src/main/java/com/demo/pages/LoginPage.java
@@ -5,19 +5,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
+
 public class LoginPage extends PageObjects <LoginPage> {
     private static final String URL = "https://www.dice.com/dashboard/login";
 
     private By emailField = By.xpath("//*[@id=\"email\"]");
     private By passwordField = By.xpath("//*[@id=\"password\"]");
     private By signInButton = By.xpath("//*[@id=\"loginDataSubmit\"]/div[3]/div/button");
+    private By errorMessage = By.xpath("//span[@data-automation-id='login-failure-help-message']");
 
 
-     public LoginPage(WebDriver driver) {
+    public LoginPage(WebDriver driver) {
 
         super(driver);
     }
-    public void openLogonPage(){
+    public void openLogInPage(){
          getPage(URL);
     }
     public void fillUpEmailAndPassword(String email, String password){
@@ -29,6 +31,10 @@ public class LoginPage extends PageObjects <LoginPage> {
         System.out.println("Clicking on Sign In Button");
         click(signInButton);
         return new ProfilePage(driver);
+    }
+    public String getLoginErrorMessage(){
+        waitForVisibilityOf(errorMessage, 15);
+        return getText(errorMessage);
     }
 
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,13 +1,15 @@
 import com.demo.base.BaseTest;
+import com.demo.base.CsvDataProvider;
 import com.demo.pages.LoginPage;
 import com.demo.pages.ProfilePage;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class LoginTest extends BaseTest {
-
-
-
 
     @Test
     public void positiveLogin() {
@@ -15,7 +17,7 @@ public class LoginTest extends BaseTest {
         String expectedPageTitle = "Seeker Dashboard - Profile";
         String correctProfileName = "Igor Polnik";
 
-        loginPage.openLogonPage();
+        loginPage.openLogInPage();
 
         loginPage.fillUpEmailAndPassword("ipolnik@yahoo.com", "456456pro");
         // push profile button and wait page to load
@@ -31,4 +33,30 @@ public class LoginTest extends BaseTest {
         // Verify correct profile
         Assert.assertTrue(profilePage.isCorrectProfileLoaded(correctProfileName), "Profile name is not expected");
     }
+
+    @Test (dataProvider = "CsvDataProvider", dataProviderClass = CsvDataProvider.class)
+    public void negativeLogin(Map<String, String> testData) {
+        String expectedErrorMessage = "Email and/or password incorrect";
+        String testNumber = testData.get("No");
+        String email = testData.get("email");
+        String password = testData.get("password");
+        String description = testData.get("Description");
+
+        System.out.println("Test number: " + testNumber + "n\for " + description + "Wheren\nemail " + email + "And password\n" + password);
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openLogInPage();
+
+        loginPage.fillUpEmailAndPassword(email, password);
+        // push sign in button
+          loginPage.pushSignInButton();
+
+          String errorMessage = loginPage.getLoginErrorMessage();
+
+        Assert.assertTrue(errorMessage.contains(expectedErrorMessage), "Error message is not expected.\nExpected"
+                + expectedErrorMessage + "\nActual" + errorMessage);
+
+}
+
+
 }
